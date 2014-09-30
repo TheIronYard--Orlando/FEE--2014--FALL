482ec979c290ec3b4ac379ab72add1e815ba8ebd
David Rogers <david@orlandophp.org>
2014-09-30 15:13:34 -0400

    Ping-pong example with @bgates and @al-the-x

--- Dojos/2014-09-30.js
+++ Dojos/2014-09-30.js
@@ -1,76 +1,42 @@
 /**
- * =   : Assignment Operator "assigned", "assigns"
- *      var foo = "bar";
+ * String Calculator
  *
- * Comparison Operators:
- * ==  : Roughly Equivalent: "double equal"
- * === : Strictly Equivalent
- * >   : "greater than"
- * <   : "less than"
- * >=  : "greater-or-equal-to"
- *
- * Boolean / Logical Operators:
- * &&  : "and" -- returns TRUE if both sides are TRUE
- * ||  : "or"  -- returns TRUE if either side is TRUE
- *
+ * Given two strings like "one", "two", and "three" that
+ * represent numbers, calculate the product of those numbers
+ * and return the result.
  */
-function lettersToNumbers(letters){
-    if (letters == "one"){
-        letters = 1;
+
+var multiply = function(a, b) {
+  if ( a === "zero" || b === "zero"){
+    return 0;
+  }
+  // if ( a === "one" && b === "zero" )
+  // if ( a === "one" && b === "one" )
+  // if ( a === "one" && b === "two" )
+  // if ( a === "one" && b === "three" )
+  if ( a === "one" ){
+    if ( b === "two" ){
+      return 2;
     }
-    if (letters == "two"){
-        letters = 2;
+
+    if ( b === "three" ){
+      return 3;
     }
-    if (letters == "three"){
-        letters = 3;
-    }
-    if (letters == "four"){
-        letters = 4;
-    }
-    if (letters == "five"){
-        letters = 5;
-    }
-    return letters;
+  }
+  if (a === "two" && b === "one" ){
+    return 2;
+  }
+  return 1;
 }
-
-function test_lettersToNumbers(a, b){
-    console.log('it should convert "' + a + '" to ' + b + ': ',
-        lettersToNumbers(a) === b);
-}
-
-test_lettersToNumbers('one', 1);
-test_lettersToNumbers('two', 2);
-test_lettersToNumbers('three', 3);
-test_lettersToNumbers('four', 4);
-test_lettersToNumbers('five', 5);
-
-function add(a, b){
-    a = lettersToNumbers(a);
-
-    b = lettersToNumbers(b);
-
-    return a + b;
-}
-
-function test_add(a, b, c){
-    console.log('it should add ' + a + ' and ' + b + ': ',
-        add(a, b) === c);
-}
-
-test_add("one", "one", 2);
-test_add("one", "two", 3);
-test_add("one", "three", 4);
-test_add("one", "four", 5);
-test_add("one", "five", 6);
-test_add("two", "one", 3);
-test_add("two", "two", 4);
-test_add("two", "three", 5);
-test_add("two", "four", 6);
-test_add("two", "five", 7);
-test_add("three", "one", 4);
-test_add("three", "two", 5);
-test_add("three", "three", 6);
-test_add("three", "four", 7);
-test_add("three", "five", 8);
-test_add("four", "one", 5);
-test_add("four", "two", 6);
+console.log('it should multiply "one" and "one": ',
+  multiply("one", "one") === 1);
+console.log('it should multiply "zero" and "one": ',
+  multiply("zero", "one") === 0);
+console.log('it should multiply "one" and "zero": ',
+  multiply("one", "zero") === 0);
+console.log('it should multiply "one" and "two": ',
+  multiply("one", "two") === 2);
+console.log('it should multiply "two" and "one": ',
+  multiply("two", "one") === 2);
+console.log('it should multiply "one" and "three": ',
+  multiply("one", "three") === 3);
