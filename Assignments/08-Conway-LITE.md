5d2e77103230819ed2e77351f8469d0d6621a4dc
David Rogers <david@orlandophp.org>
2014-10-02 15:42:26 -0400

    Adding assignment 08 and corresponding Dojo

---
+++ Assignments/08-Conway-LITE.md
@@ -0,0 +1,17 @@
+* Resources:
+  * [Conway's Game of Life](http://en.wikipedia.org/wiki/Conway's_Game_of_Life)
+  * [Code School: JS Road Trip 2](https://www.codeschool.com/courses/javascript-road-trip-part-2)
+    * Level 1: Labyrinth of Loops
+    * Level 2: Conditional Canyon
+    * Level 5: Array Archipelago
+* Coding Dojo
+  * Complete [`Dojos/2014-10-02.js`](/TheIronYard--Orlando/FEE--2014--FALL/blob/master/Dojos/2014-10-02.js)
+  * To ensure that _both_ partners have a working solution, trade off when you pair!
+  * Turn-in by opening a PR in your `TIY-Assignments` repo as usual.
+* Github Pages
+  * Some teammates have work still left on the Github Pages assignment from last week.
+  * Make sure that _everyone_ is caught up by Monday!
+* Read the entire assignment...
+  * Make your _own_ checkboxes for these items.
+  * You should have at least 2, but more is better...!
+
---
+++ Dojos/2014-10-02.js
@@ -0,0 +1,136 @@
+/**
+ * Conway's Game of Life (LITE)
+ *
+ * @link http://en.wikipedia.org/wiki/Conway's_Game_of_Life
+ *
+ * Consider a finite 3x3 board of cells:
+ *
+ *     0   1   2
+ *   +---+---+---+
+ * 0 |   |   |   |
+ *   +---+---+---+
+ * 1 |   |   |   |
+ *   +---+---+---+
+ * 2 |   |   |   |
+ *   +---+---+---+
+ *
+ * Represented as:
+ *
+ * board = [
+ *   [ false, false, false ],
+ *   [ false, false, false ],
+ *   [ false, false, false ],
+ * ]
+ *
+ * Where each Boolean value represents the state of a `cell`, either
+ * alive (`true`) or dead (`false`). Each cell can be accessed with
+ * the index operators (`[n]`), e.g. `board[0][1]`, `board[2][2]`.
+ *
+ * Write a function called `conway` that...
+ *
+ * - GIVEN the state of a `cell` and a list of neighboring cells...
+ * - THEN returns the state of the `cell` with the rules applied.
+ *
+ * Write a function called `neighborsOf` that...
+ *
+ * - GIVEN a `board` and `row` and `col` coordinates...
+ * - THEN returns a list of the neighboring cells.
+ *
+ * Write a function called `tick` that...
+ *
+ * - GIVEN a `board`...
+ * - THEN returns a NEW `board` representing the next generation.
+ *
+ * Use the provided `board` function to produce the initially empty
+ * board, and test the following starting positions:
+ *
+ * Initially Empty Board
+ * =====================
+ *
+ *     0   1   2          0   1   2
+ *   +---+---+---+      +---+---+---+
+ * 0 |   |   |   |    0 |   |   |   |
+ *   +---+---+---+      +---+---+---+
+ * 1 |   |   |   | >> 1 |   |   |   |
+ *   +---+---+---+      +---+---+---+
+ * 2 |   |   |   |    2 |   |   |   |
+ *   +---+---+---+      +---+---+---+
+ *
+ * Just One Cell
+ * =============
+ *
+ *     0   1   2          0   1   2
+ *   +---+---+---+      +---+---+---+
+ * 0 |   |   |   |    0 |   |   |   |
+ *   +---+---+---+      +---+---+---+
+ * 1 |   | X |   | >> 1 |   |   |   |
+ *   +---+---+---+      +---+---+---+
+ * 2 |   |   |   |    2 |   |   |   |
+ *   +---+---+---+      +---+---+---+
+ *
+ * Two Cells in Any Arrangement
+ * ============================
+ *
+ *     0   1   2          0   1   2
+ *   +---+---+---+      +---+---+---+
+ * 0 |   |   |   |    0 |   |   |   |
+ *   +---+---+---+      +---+---+---+
+ * 1 |   | X | X | >> 1 |   |   |   |
+ *   +---+---+---+      +---+---+---+
+ * 2 |   |   |   |    2 |   |   |   |
+ *   +---+---+---+      +---+---+---+
+ *
+ *     0   1   2          0   1   2
+ *   +---+---+---+      +---+---+---+
+ * 0 | X |   |   |    0 |   |   |   |
+ *   +---+---+---+      +---+---+---+
+ * 1 |   | X |   | >> 1 |   |   |   |
+ *   +---+---+---+      +---+---+---+
+ * 2 |   |   |   |    2 |   |   |   |
+ *   +---+---+---+      +---+---+---+
+ *
+ * Three Cells in a Line
+ * =====================
+ *       start              tick 1             tick 2
+ *     0   1   2          0   1   2          0   1   2
+ *   +---+---+---+      +---+---+---+      +---+---+---+
+ * 0 |   | X |   |    0 |   |   |   |    0 |   | X |   |
+ *   +---+---+---+      +---+---+---+      +---+---+---+
+ * 1 |   | X |   | >> 1 | X | X | X | >> 1 |   | X |   |
+ *   +---+---+---+      +---+---+---+      +---+---+---+
+ * 2 |   | X |   |    2 |   |   |   |    2 |   | X |   |
+ *   +---+---+---+      +---+---+---+      +---+---+---+
+ *
+ * You may test more starting position, if you like, of course.
+ */
+
+var assert = require('assert');
+
+/**
+ * Log `success` if `actual` is STRICTLY equal to `expected`
+ *
+ * @param ANY actual
+ * @param ANY expected
+ * @param String success
+ */
+function test(actual, expected, success){
+    if (success === undefined) success = 'pass!';
+
+    assert.strictEqual(actual, expected);
+
+    console.log(success);
+}
+
+/**
+ * Return a nested array representing an initially empty 3x3 board.
+ *
+ * @return Array of Array of Boolean
+ */
+function board(){
+    return [
+        [ false, false, false ],
+        [ false, false, false ],
+        [ false, false, false ],
+    ];
+}
+
