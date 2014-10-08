f41eb0c0704a37c595654c142afa1464a645acd4
Ally Hinton <allyhinton383@gmail.com>
2014-10-08 21:50:03 +0000

    Got my top variables ready to go

--- Assignments/07--CheckWriting_Xx_Nova_xX_.js
+++ Assignments/07--CheckWriting_Xx_Nova_xX_.js
@@ -1,59 +1,10 @@
 /** ==== CODE! ==== **/
-function num2Words(dollars){
-    var suffix = " dollars";
-    var suffix2 = "twenty"
+function num2Words(money)
 
-    /**
-     * 1. "one"
-     * 2. "two"
-     * 3. "three"
-     * 4. "four"
-     * 5. "five"
-     */
-    var ones =  [ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ]
-    var teens = [ "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" ];
+var value = money.toString();// takes input and turns it into a string. Ex = 1 turns to "1"
+value = Number(value).toFixed(2); //takes the string from above and wraps it in Number wrapper(tells that it's a number) 
+                                  //and makes sure it has 2 decimal points but no more. Ex = "1" = "1.00"
+var cents = value.slice(-2); //creates a cents variable so that I can seperate the cents, which does not need to be changed in the output,
+                            //and hold it for when it is called. Ex "10.25" = "25"
+var centsWords = "dollars & " + cents + "/100s"; // creates a variable to use for printout of string to save typing Ex 1.25 = "one dollars & 25/100s
 
-    if (dollars >= 10){
-        return teens[dollars - 10] + suffix;
-    }
-    return ones[dollars] + suffix;
-    
-    if (dollars >= teens){
-        return ones[dollars] + suffix2;
-    }
-}
-
-/** ==== TESTS! ==== **/
-var assert = require('assert');
-
-function testHelper(number, string, message){
-    if ( message === undefined ) message = 'pass: ' + string;
-
-    assert.strictEqual(num2Words(number), string);
-    console.log(message);
-}
-
-assert(num2Words);
-console.log('got here!');
-
-
-testHelper(1, "one dollars");
-testHelper(2, "two dollars");
-testHelper(3, "three dollars");
-testHelper(4, "four dollars");
-testHelper(5, "five dollars");
-testHelper(6, "six dollars");
-testHelper(7, "seven dollars");
-testHelper(8, "eight dollars");
-testHelper(9, "nine dollars");
-
-testHelper(10, "ten dollars");
-testHelper(11, "eleven dollars");
-testHelper(12, "twelve dollars");
-testHelper(13, "thirteen dollars");
-testHelper(14, "fourteen dollars");
-testHelper(15, "fifteen dollars");
-testHelper(16, "sixteen dollars");
-testHelper(17, "seventeen dollars");
-testHelper(18, "eighteen dollars");
-testHelper(19, "nineteen dollars");
