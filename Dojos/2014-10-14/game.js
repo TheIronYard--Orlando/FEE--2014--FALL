7f57a611d42474cc61f4438f9c8a5344e86d7f8d
David Rogers <david@orlandophp.org>
2014-10-14 16:27:17 -0400

    Final examples!

--- Dojos/2014-10-14/game.js
+++ Dojos/2014-10-14/game.js
@@ -1,90 +1,97 @@
-var privates = {
-  board: undefined,
-  newBoard: function(){
-    return [
-      [ false, false, false ],
-      [ false, false, false ],
-      [ false, false, false ],
-    ]
-  },
-  rules: function(cell, neighbors){ /* i.e. conway(cell, neighbors) */ },
-  neighborsOf: function(x,y){ /* . . . */ },
-  tick: function(){
-    if ( this.board[0][1] && this.board[1][1] && this.board[2][1] ){
-        this.board[0][1] = false;
-        this.board[2][1] = false;
+module.exports = Game;
 
-        this.board[1][0] = true;
-        this.board[1][2] = true;
-
-    }
-
-    else if ( this.board[1][0] && this.board[1][1] && this.board[1][2] ){
-      this.board[1][0] = this.board[1][2] = false;
-      this.board[0][1] = this.board[2][1] = true;
-
-    }
-  }
+function board(){
+  return [
+    [ false, false, false ],
+    [ false, false, false ],
+    [ false, false, false ],
+  ];
 }
 
-module.exports = {
-    /**
-     * @param Number x coordinate
-     * @param Number y coordinate
-     * @return Boolean representation of a cell
-     */
-    getCell: function(){
+function Game(){
+  this.board = board();
+}
 
-    }
-    /**
-     * @param Number x coordinate
-     * @param Number y coordinate
-     * @return Boolean if cell at {x,y} is alive
-     */
-    isAlive: function(x, y){
+/**
+ * @param Number x coordinate
+ * @param Number y coordinate
+ * @return Boolean if cell at {x,y} is alive
+ */
+Game.prototype.isAlive = function(x, y){
 
-    },
-    /**
-     * @param Number x coordinate
-     * @param Number y coordinate
-     * @return NUTHIN!?
-     */
-    makeLive: function(){
+}
 
-    }
-    /**
-     * WARNING: This is VOODOO MAGIC...
-     *
-     * GIVEN:
-     *   this.board === [
-     *      [ false, true,  false ],
-     *      [ false, true,  false ],
-     *      [ false, true,  false ],
-     *   ];
-     *
-     * EXPECT:
-     *   +---+---+---+
-     *   |   | X |   |
-     *   +---+---+---+
-     *   |   | X |   |
-     *   +---+---+---+
-     *   |   | X |   |
-     *   +---+---+---+
-     */
-    display: function(){
-        var spacer = '+---+---+---+\n';
+/**
+ * Make the cell at {x,y} "alive", whatever that means...
+ *
+ * @param Number x coordinate
+ * @param Number y coordinate
+ * @return undefined
+ */
+Game.prototype.setAlive = function(x, y){
 
-        return spacer +
-            // Apply `renderRow` to each `row` in `board`...
-            this.board.map(function renderRow(row){
-                return '| ' +
-                    // Apply `renderCell` to each `cell` in `row`...
-                    row.map(function renderCell(cell){
-                        // return 'X' if `cell` is TRUTHY otherwise return ' '
-                        return cell && 'X' || ' ';
-                    }).join(' | ') // Place ' | ' between each `cell`...
-                + ' |\n';
-            }).join(spacer) // Place `spacer` between each `row`...
-        + spacer;
-    } // END display
-} // END game
+}
+
+/**
+ * Make the cell at {x,y} "dead", whatever that means...
+ *
+ * @param Number x coordinate
+ * @param Number y coordinate
+ * @return undefined
+ */
+Game.prototype.setDead = function(x, y){
+
+}
+
+/**
+ * Update the `board` by applying the `rules` to each cell.
+ */
+Game.prototype.tick = function(){
+  // Start with a fresh board...
+  // Apply `rules` to each cell in the current board...
+  // Record the result of `rules` in the new board...
+  // Update the current board to match the new board.
+}
+
+/**
+ * What goes here?
+ */
+Game.prototype.rules = function(){
+
+}
+
+/**
+ * WARNING: This is VOODOO MAGIC...
+ *
+ * GIVEN:
+ *   this.board === [
+ *      [ false, true,  false ],
+ *      [ false, true,  false ],
+ *      [ false, true,  false ],
+ *   ];
+ *
+ * EXPECT:
+ *   +---+---+---+
+ *   |   | X |   |
+ *   +---+---+---+
+ *   |   | X |   |
+ *   +---+---+---+
+ *   |   | X |   |
+ *   +---+---+---+
+ */
+Game.prototype.display = function(){
+    var spacer = '+---+---+---+\n';
+
+    return spacer +
+        // Apply `renderRow` to each `row` in `board`...
+        this.board.map(function renderRow(row){
+            return '| ' +
+                // Apply `renderCell` to each `cell` in `row`...
+                row.map(function renderCell(cell){
+                    // return 'X' if `cell` is TRUTHY otherwise return ' '
+                    return cell && 'X' || ' ';
+                }).join(' | ') // Place ' | ' between each `cell`...
+            + ' |\n';
+        }).join(spacer) // Place `spacer` between each `row`...
+    + spacer;
+} // END display
--- Dojos/2014-10-14/game_test.js
+++ Dojos/2014-10-14/game_test.js
@@ -1,46 +1,25 @@
-var game = require('./game.js');
+var Game = require('./game.js');
 
 var assert = require('chai').assert;
 
-it('should have a game interface', function(){
-  assert(game);
-  assert.isFunction(game.newBoard);
-  assert.isFunction(game.rules);
-  assert.isFunction(game.neighborsOf);
-  assert.isFunction(game.tick);
-  assert.isFunction(game.display);
-});
-
-// /*
-it('should create an empty board', function(){
-  assert.deepEqual(game.newBoard(), [
-    [ false, false, false ],
-    [ false, false, false ],
-    [ false, false, false ],
-  ]);
-});
-// */
-
 it('should play the game', function(){
-  // game.board = game.newBoard();
+  var game = new Game;
 
-  assert.isDefined(game.board);
-  assert.isArray(game.board);
-  assert.lengthOf(game.board, 3);
   assert.deepEqual(game.board, [
-      [ false, false, false ],
-      [ false, false, false ],
-      [ false, false, false ],
+    [ false, false, false ],
+    [ false, false, false ],
+    [ false, false, false ],
   ]);
 
-  // game.board[0][1] = game.board[1][1] = game.board[2][1] = true;
-  // game.board[1][0] = game.board[1][2] = true;
-
-  game.makeLive(0, 1);
-  game.makeLive(1, 1);
-  game.makeLive(2, 1);
+/* Uncomment me to kill tests...
+  game.setAlive(0,1);
+  game.setAlive(1,1);
+  game.setAlive(2,1);
 
   assert.isTrue(game.isAlive(0, 1));
+  assert.isTrue(game.isAlive(1, 1));
+  assert.isTrue(game.isAlive(2, 1));
+
   assert.equal(game.display(),
     "+---+---+---+\n" +
     "|   | X |   |\n" +
@@ -52,6 +31,10 @@ it('should play the game', function(){
   );
 
   game.tick();
+
+  assert.isTrue(game.isAlive(1,0));
+  assert.isTrue(game.isAlive(1,1));
+  assert.isTrue(game.isAlive(1,2));
 
   assert.equal(game.display(),
     "+---+---+---+\n" +
@@ -65,6 +48,10 @@ it('should play the game', function(){
 
   game.tick();
 
+  game.isTrue(game.isAlive(0,1));
+  game.isTrue(game.isAlive(1,1));
+  game.isTrue(game.isAlive(2,1));
+
   assert.equal(game.display(),
     "+---+---+---+\n" +
     "|   | X |   |\n" +
@@ -74,5 +61,5 @@ it('should play the game', function(){
     "|   | X |   |\n" +
     "+---+---+---+\n"
   );
-
+  // */ // Don't uncomment this line!
 });
