bded84921212c0da088f1e21b5e1953634305824
David Rogers <al.the.x@gmail.com>
2014-10-20 16:24:17 +0000

    @arigonzo: Look we wrote tests!

--- Dojos/2014-10-20/chess_test.js
+++ Dojos/2014-10-20/chess_test.js
@@ -1,13 +1,17 @@
-var Chess = require('./chess.js');
-// var Position = require("./chess.js");
+var Chess = require('./chess.js').Chess;
+var Position = require("./chess.js").Position;
 console.log('This is the result of `require("./chess")`: ', Chess);
 
 var assert = require('chai').assert;
 
 describe('Position', function(){
+    it('should have a function Position()', function(){
+        assert.isFunction(Position, "STEPID!!");
+    });
 });
 
 describe('Piece', function(){
+    it('should have a function Piece()'
 });
 
 describe('Chess', function(){
