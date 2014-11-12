06baffaf731d628ec1e6c34616cff7b87d0c6854
David Rogers <david@orlandophp.org>
2014-11-12 00:21:13 -0500

    Refactoring for BrowserStack...

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
 var gulp = require('gulp'),
     connect = require('gulp-connect');
 
-gulp.task('connect', function(){
-  connect.server({
-    livereload: true
+{ /* === Vanilla Connect === */
+  gulp.task('connect', function(){
+    connect.server({
+      livereload: true
+    });
   });
-});
 
-gulp.task('reload', function(){
-  gulp.src('./Dojos/**/*.*')
-    .pipe(connect.reload());
-});
+  gulp.task('reload', function(){
+    gulp.src('./Dojos/**/*.*')
+      .pipe(connect.reload());
+  });
 
-gulp.task('watch', function(){
-  gulp.watch(['./Dojos/**/*.*'], ['reload']);
-});
+  gulp.task('watch', function(){
+    gulp.watch(['./Dojos/**/*.*'], ['reload']);
+  });
+} // END Vanilla Connect
 
 gulp.task('default', ['connect', 'watch']);
 
