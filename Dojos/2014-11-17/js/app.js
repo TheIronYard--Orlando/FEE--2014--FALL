23de2201c9aaedef1eea2a560032d6d7ee4803c8
David Rogers <david@orlandophp.org>
2014-11-17 14:11:16 -0500

    Behold the power of Angular JS!
    Yeah, ignore the mess...

--- Dojos/2014-11-17/js/app.js
+++ Dojos/2014-11-17/js/app.js
@@ -2,5 +2,24 @@
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
+  angular.module('todomvc', [ ])
+    .controller('Ctrl', function(){
+      this.tasks = [
+        { text: 'thing in controller 1' },
+        { text: 'thing in controller 2' },
+      ];
+
+      this.startEdit = function($event, task){
+        console.log($event);
+        task.editing = true;
+      }
+
+      this.cancelEdit = function($event, task){
+        if ( $event.keyCode === 27 ){
+          task.editing = false;
+        }
+      }
+    })
+  ; // END module(todomvc)
 
 })( window );
