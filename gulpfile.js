8099d0cd1078de3c88061337a180402fee1c6323
David Rogers <david@orlandophp.org>
2014-11-17 08:22:44 -0500

    Open the connect site by default...

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    connect = require('gulp-connect'),
+    open = require('opn');
 
 { /* === Vanilla Connect === */
   gulp.task('connect', function(){
     connect.server({
+      directory: true,
       livereload: true
     });
   });
@@ -18,5 +20,7 @@ var gulp = require('gulp'),
   });
 } // END Vanilla Connect
 
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', ['connect', 'watch'], function(){
+  open('http://localhost:8080');
+});
 
