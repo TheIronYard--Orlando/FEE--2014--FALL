b5978bc7a02e438ab1b827b49904b098662ae5ed
David Rogers <david@orlandophp.org>
2014-11-20 11:08:44 -0500

    Generated by `generator-gulp-angular`...

---
+++ Dojos/2014-11-20/src/app/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('20141120', ['ngCookies', 'restangular', 'ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('/');
+  })
+;
---
+++ Dojos/2014-11-20/src/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('20141120')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
